84287c38efafe8d29dbce16ab6dfa55a09f1ae73
VJAI <vijay.prideparrot@gmail.com>
2016-09-28 15:00:05 +0530

    Code refactoring in waiter

--- waiter.js
+++ waiter.js
@@ -1,25 +1,27 @@
 (function (W, D) {
   
-  var Qy = D.body.querySelector.bind(D.body), manual = Qy('script').getAttribute('data-manual');
+  var Qy = D.querySelector.bind(D), manual = D.body.getAttribute('data-manual');
   
-  D.write('<div class="waiter anime">\
-							<div class="logo"></div>\
-							<div class="progress-bar">\
-								<div class="progress"></div>\
-							</div>\
-				 	 </div>');
+  D.body.classList.add('no-scroll');
+  D.write('<div class="waiter waiter--anime"><div class="waiter__logo"></div><div class="waiter__progressbar"><div class="waiter__progress waiter__progress--anime0"></div></div></div>');
   
   var waitOver = function () {
-    var waiter = Qy('.waiter'), progress = Qy('.progress');
+    var waiter = Qy('.waiter'), progress = Qy('.waiter__progress'), t = 500;
     
     setTimeout(function () {
-      progress.style.animation = 'none';
-      progress.style.width = '100%';
+      var progressWidth = W.getComputedStyle(progress, null).getPropertyValue('width');
+      progress.classList.remove('waiter__progress--anime0');
+      progress.style.width = progressWidth;
+      progress.classList.add('waiter__progress--anime1');
       
       setTimeout(function () {
-        waiter.classList.add('trigger');
-      }, 500);
-    }, 500);
+        waiter.addEventListener('transitionend', function () {
+          D.body.classList.remove('no-scroll');
+        }, false);
+        
+        waiter.classList.add('waiter--anime-trigger');
+      }, t);
+    }, t);
   };
   
   manual === 'true' ? (W.waitOver = waitOver) : W.addEventListener('load', waitOver, false);
